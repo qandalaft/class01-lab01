474bf7cea16c375e6d5e93c06421eae9ffb79375
qandalaft <qandalaftemad@gmail.com>
2021-09-06 22:09:17 +0300

    push the react lab01

---
+++ myapp/src/components/Footer .js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <p>All rights reserved<sup>@</sup>
+                </p>
+            </div>
+        )
+    }
+}
+
+export default Footer
